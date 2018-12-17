d7ee4afd768403461a7eb95e91f50a21ba0f07bd
ye970915 <783671475@qq.com>
2018-12-17 21:14:14 +0800

    second开始

--- js/common.js
+++ js/common.js
@@ -1,5 +1,18 @@
 $(function () {
     $('.lt_list ul li:nth-child(2)').click(function () {
         $('.child').stop().slideToggle();
-    })
+    });
+
 });
+NProgress.configure({ showSpinner: false });
+$(document).ajaxStart(function () {
+    // console.log(1);
+
+    NProgress.start();
+});
+
+$(document).ajaxStop(function () {
+    // console.log(2);
+
+    NProgress.done();
+})
---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(function () {
+    $(".top_a_left").click(function () {
+        $('.lt_aside').toggleClass('now');
+        $('.lt_left').toggleClass('now')
+    });
+    
+});
+
+
